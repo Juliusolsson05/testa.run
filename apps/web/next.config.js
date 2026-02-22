e51382385104bf6daa2675f3388aed6e676f7eb3
Julius Olsson <julius.olsson05@gmail.com>
2026-02-22 01:29:00 +0100

    fix(web): allow screenshot host for Next Image remote patterns

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '76.13.77.252',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
